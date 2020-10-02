9028e79cb7e4c769c80b1992564e22ea96ac9e61
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-02 23:05:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,6 +11,7 @@ class StudentsController < ApplicationController
 
   def activate
     @student=Student.find(params[:id])
+    binding.pry
     if @student.active != true
       @student.active = true
       @student.save
